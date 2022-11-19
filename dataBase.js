07448aa1a62db24a9f8b6c701a74b57b660eca0a
Eliel Maia <elieljuniorm@gmail.com>
2022-11-19 19:29:06 -0300

    entrada de dados

--- dataBase.js
+++ dataBase.js
@@ -1,27 +1,52 @@
-export const profissionais = [
-    {
-        "profissioal": "Eliel Maia",
-        "cpf": "74773217006"
-    }
-];
-export const especialidades = [
-    {
-        "especialista": [
-            "Cardiologista",
-            "Fisioterapeuta"
-        ]
-    }
-];
-export const estados = [
-    {
-        "estado": [
-            "Pará",
-            "São Paulo",
-            "Rio de Janeiro",
-            "Maranhão"
-        ]
-    }
-];
+export const profissionais = {
+    "profissional": [
+        {
+            "nome": {
+                "Mario Lima Nunes": {
+                    "cpf": "74773217006"
+                }
+            },
+        },
+        {
+            "nome": {
+                "Jorge Augusto dos Santos": {
+                    "cpf": "60627576079"
+                }
+            },
+        },
+        {
+            "nome": {
+                "Yuri Alberto Costa": {
+                    "cpf": "60627576079"
+                }
+            },
+        },
+        {
+            "nome": {
+                "Tamile Costa Moura": {
+                    "cpf": "87284871091"
+                }
+            },
+        },
+    ]
+};
+
+export const especialidades = {
+    "especialista": [
+        "Cardiologista",
+        "Fisioterapeuta"
+    ]
+};
+
+export const estados = {
+    "estado": [
+        "Pará",
+        "São Paulo",
+        "Rio de Janeiro",
+        "Maranhão"
+    ]
+};
+
 export const cidades = [
     {
         "Pará": [
@@ -43,4 +68,4 @@ export const cidades = [
             "Santo André",
         ]
     }
-]
+];
