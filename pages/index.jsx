81591762a586d354cc69883fb3d802b697c9241e
Eliel Maia <elieljuniorm@gmail.com>
2022-11-19 16:45:41 -0300

    limpando projeto

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1>API Fácil Consulta - Link util</h1>
+      <a href="http://localhost:3000/api/hello"  style={{fontSize: "2em", textDecoration: "none"}}>Consulte a API</a>
+    </div>
+  )
+}
+
+export default index
