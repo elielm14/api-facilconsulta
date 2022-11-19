edc34b647305582f7f90c3be44be78c41b224490
Eliel Maia <elieljuniorm@gmail.com>
2022-11-19 16:48:23 -0300

    ajustando css

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function index() {
   return (
-    <div style={{textAlign: "center"}}>
+    <div style={{textAlign: "center", margin: "10%"}}>
       <h1>API Fácil Consulta - Link util</h1>
-      <a href="http://localhost:3000/api/hello"  style={{fontSize: "2em", textDecoration: "none"}}>Consulte a API</a>
+      <a href="http://localhost:3000/api/data"  style={{fontSize: "2em", textDecoration: "none"}}>Consulte a API</a>
     </div>
   )
 }
